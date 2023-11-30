4a5602da2e44e38e43627e06f37f0aeb6bc5fcdd
mergd <24722014+mergd@users.noreply.github.com>
2023-11-29 17:14:01 -0800

    feat: add not-found page, and add additional utils

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import { SITE_DESCRIPTION } from '@/utils/site'
+
+export default function Home() {
+  return (
+    <>
+      <h2 className='text-lg'>Error 404 Page Not Found</h2>
+    </>
+  )
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,31 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import { parseUnits } from 'viem'
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatNumber(number: number, decimals = 2) {
+  return number.toLocaleString(undefined, {
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals,
+  })
+}
+
+export function parseLocalizedValue(amount: string, decimals = 18) {
+  const cleaned = amount.replace(/[^\d.-]/g, '')
+  return parseUnits(cleaned, decimals)
+}
+
+export function debounce<T extends (...args: any[]) => any>(func: T, wait = 100) {
+  let timeout: ReturnType<typeof setTimeout> | undefined
+  return function (this: any, ...args: Parameters<T>) {
+    const later = () => {
+      timeout = undefined
+      func.apply(this, args)
+    }
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+  } as T
 }
